b4284bf59f5795ad168c9a0eaf6ff5b71f123b58
Fakecrafter <Fakecrafter@protonmail.com>
2022-09-23 21:40:05 +0200

    minor changes

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
+from snake import Snake
 # Importieren der Pygame-Bibliothek
 import pygame
+
+
+def drawGrid(blockSize):
+    for x in range(0, 1000, blockSize):
+        for y in range(0, 1000, blockSize):
+            rect = pygame.Rect(x, y, blockSize, blockSize)
+            pygame.draw.rect(screen, GREY, rect, 1)
 
 # initialisieren von pygame
 pygame.init()
 
 # genutzte Farbe
-ROT     = ( 255, 0, 0)
-GRUEN   = ( 0, 214, 0)
-SCHWARZ = ( 0, 0, 0)
+RED     = ( 255, 0, 0)
+GREEN   = ( 0, 214, 0)
+BLACK   = ( 0, 0, 0)
+WHITE   = (255, 255, 255)
+GREY    = (100, 100, 100)
+
+
+speed = 10
+blockSize = 40
 
 
 # Fenster öffnen
-screen = pygame.display.set_mode((640, 480))
+screen = pygame.display.set_mode((1004, 1004))
 pygame.display.set_caption("Snake")
+
+# instanz der Snake kreieren
+mySnake = Snake()
 
 # solange die Variable True ist, soll das Spiel laufen
 spielaktiv = True
@@ -21,34 +38,54 @@ spielaktiv = True
 clock = pygame.time.Clock()
 
 # Schleife Hauptprogramm
-while spielaktiv:
+while(spielaktiv):
     # Überprüfen, ob Nutzer eine Aktion durchgeführt hat
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             spielaktiv = False
-
-
-            elif event.key == pygame.K_w:
-                print("Spieler hat Taste w gedrückt")
+        # uberpruefe tastatureingaben
+        # je nach eingabe aendere richtung der snake
+        elif event.type == pygame.KEYDOWN:
+            safedx = mySnake.dx
+            safedy = mySnake.dy
+            if event.key == pygame.K_d:
+                mySnake.dx = 1
+                mySnake.dy = 0
             elif event.key == pygame.K_a:
-                print("Spieler hat Taste a gedrückt")
+                mySnake.dx = -1
+                mySnake.dy = 0
             elif event.key == pygame.K_s:
-                print("Spieler hat Taste s gedrückt")
-            elif event.key == pygame.K_d:
-                print("Spieler hat Taste d gedrückt")
+                mySnake.dx = 0
+                mySnake.dy = 1
+            elif event.key == pygame.K_w:
+                mySnake.dx = 0
+                mySnake.dy = -1
+            if (-1 * safedx == mySnake.dx):
+                mySnake.dx = safedx
+            if (-1 * safedy == mySnake.dy):
+                mySnake.dy = safedy
 
 
     # Spiellogik hier integrieren
+    mySnake.movement()
+    mySnake.checkCollisionWall()
+    mySnake.checkCollisionSelf()
+    mySnake.checkCollisionItem()
+
 
     # Spielfeld löschen
-    screen.fill(SCHWARZ)
+    screen.fill(BLACK)
+    drawGrid(blockSize)
 
     # Spielfeld/figuren zeichnen
+    mySnake.draw(40, screen, GREEN)
+
+    # myApple.draw()
 
     # Fenster aktualisieren
     pygame.display.flip()
 
     # Refresh-Zeiten festlegen
-    clock.tick(60)
+    clock.tick(speed)
 
 pygame.quit()
--- snake.py
+++ snake.py
@@ -1,30 +1,37 @@
 import random
 import pygame
 
-class snake():
-	head = []
-	body = [] # beinhaltet kopf
-	dy = 0
-	dx = 0
-	
-	
-	def draw(self):
-		pass
-	def movement(self):
-		# snake einen block in seine richtung bewegen
-		pass
-	def appendBlock(self):
-		# snake laenge um einen block verlaengern
-		pass
-	def checkCollisionItem(self):
-		# Kollision mit Item z.b. apfel 
-		pass
-		return item
-	def checkCollisionWall(self):
-		# kollision mit wand 
-		pass
-	def checkCollisionSelf(self):
-		# Kollision mit sich selbst
-		pass
-	def reset(self):
-		pass
+class Snake:
+    head = []
+    body = 0 # beinhaltet kopf
+    score = 0
+    dy = 0
+    dx = 0
+
+    def __init__(self):
+        self.reset()
+    def draw(self, size, screen, color):
+        pygame.draw.rect(screen, color, pygame.Rect(self.head[0] * size, self.head[1] * size, size, size))
+    def movement(self):
+        # snake einen block in seine richtung bewegen
+        self.head[0] += self.dx
+        self.head[1] += self.dy
+
+    def appendBlock(self):
+        # snake laenge um einen block verlaengern
+        pass
+    def checkCollisionItem(self):
+        # Kollision mit Item z.b. apfel
+        pass
+    def checkCollisionWall(self):
+        # kollision mit wand
+        pass
+    def checkCollisionSelf(self):
+        # Kollision mit sich selbst
+        pass
+    def reset(self):
+        self.score = 0
+        self.dy = 0
+        self.dx = 0
+        self.body = []
+        self.head = [0, 0]
